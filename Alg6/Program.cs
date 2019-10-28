0852eb752259dbbc0d9cd4a544be79dfe95d5a41
vova chacov <vvvoha13@gmail.com>
2019-10-28 19:57:15 +0200

    Добавьте файлы проекта.

---
+++ Alg6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alg6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashTable<int> hs = new HashTable<int>(20);
+            hs.Add("mama", 5);
+            hs.Add("ababagalamaga", 7);
+            hs.Delete("mama");
+            int n = hs.Search("mama");
+            hs.Search("dasdas");
+            //Console.WriteLine(n);
+            //Console.WriteLine(115%16);
+            Console.ReadKey();
+        }
+    }
+}
